e9d768e595e25672fbd088a2129762ccbc2638f3
inateno <inateno@gmail.com>
2020-07-01 17:06:56 +0200

    fix locales and indent a bit

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const Popup = function()
         + "selector:: " + params.containerId );
     }
 
-    this.el = document.createElement( 'div' );
+    this.el = document.createElement('div');
     this.el.id = 'de-plugin-popups-container';
 
     domContainer.appendChild( this.el );
@@ -77,152 +77,166 @@ const Popup = function()
       return;
     
     var id = Date.now() + "-" + ( Math.random() * 100 >> 0 );
-    var popup = document.createElement( 'div' );
+    var popup = document.createElement('div');
       popup.innerHTML = this.template;
-      popup.getElementsByClassName( 'content' )[ 0 ].innerHTML = text;
+      popup.getElementsByClassName('content')[ 0 ].innerHTML = text;
       
-      popup.getElementsByClassName( 'buttonsPrompt' )[ 0 ].style.display = "none";
-      popup.getElementsByClassName( 'inputTextfield' )[ 0 ].style.display = "none";
-      popup.getElementsByClassName( 'buttonsTextfield' )[ 0 ].style.display = "none";
-      popup.getElementsByClassName( 'buttonsCustom' )[ 0 ].style.display = "none";
+      popup.getElementsByClassName('buttonsPrompt')[ 0 ].style.display = "none";
+      popup.getElementsByClassName('inputTextfield')[ 0 ].style.display = "none";
+      popup.getElementsByClassName('buttonsTextfield')[ 0 ].style.display = "none";
+      popup.getElementsByClassName('buttonsCustom')[ 0 ].style.display = "none";
       popup.id = 'popup' + id;
       popup.className = 'de-plugin-popup';
-      switch( type )
-      {
-        case "prompt":
-          if ( contexts && !contexts.yes )
-            contexts = { yes: contexts, no: contexts };
-          if ( !contexts )
-            contexts = { yes: window, no: window };
-          popup.getElementsByClassName( 'buttonsDefault' )[ 0 ].style.display = "none";
-          popup.getElementsByClassName( 'buttonsPrompt' )[ 0 ].style.display = "block";
-          var yes = popup.getElementsByClassName( 'yesPrompt' )[ 0 ];
-          yes.innerHTML = DE.Localization.get( "yes" ) || "Yes";
-          yes.addEventListener( 'pointerup'
+
+    switch( type )
+    {
+      case "prompt":
+        if ( contexts && !contexts.yes )
+          contexts = { yes: contexts, no: contexts };
+        if ( !contexts )
+          contexts = { yes: window, no: window };
+        popup.getElementsByClassName('buttonsDefault')[ 0 ].style.display = "none";
+        popup.getElementsByClassName('buttonsPrompt')[ 0 ].style.display = "block";
+        var yes = popup.getElementsByClassName('yesPrompt')[ 0 ];
+        yes.innerHTML = DE.Localization.get('popup.yes') || DE.Localization.get('yes') || 'Yes';
+        yes.addEventListener('pointerup'
           , function( e )
           {
             e.stopPropagation();
             e.preventDefault();
             if ( callbacks.sound_yes || callbacks.sound || _self.defaultSounds.yes || _self.defaultSounds.default )
               DE.Audio.fx.play( callbacks.sound_yes || callbacks.sound || _self.defaultSounds.yes|| _self.defaultSounds.default );
-            if ( callbacks.yes )
-              callbacks.yes.call( contexts.yes );
+            if (callbacks.yes) {
+              if (contexts.yes) {
+                callbacks.yes.call( contexts.yes );
+              } else {
+                callbacks.yes();
+              }
+            }
             _self.remove( popup.id );
             return false;
           } );
-          var no = popup.getElementsByClassName( 'noPrompt' )[ 0 ];
-          no.innerHTML = DE.Localization.get( "no" ) || "No";
-          no.addEventListener( 'pointerup'
+        var no = popup.getElementsByClassName('noPrompt')[ 0 ];
+        no.innerHTML = DE.Localization.get('popup.no') || DE.Localization.get('no') || 'No';
+        no.addEventListener('pointerup'
           , function( e )
           {
             e.stopPropagation();
             e.preventDefault();
             if ( callbacks.sound_no || callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default )
               DE.Audio.fx.play( callbacks.sound_no || callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default );
-            if ( callbacks.no )
-              callbacks.no.call( contexts.no );
+            if ( callbacks.no ) {
+              if (contexts.no) {
+                callbacks.no.call( contexts.no );
+              } else {
+                callbacks.no();
+              }
+            }
             _self.remove( popup.id );
             return false;
           } );
-          break;
-        
-        // generate a button list
-        case "custom":
-          popup.getElementsByClassName( 'buttonsDefault' )[ 0 ].style.display = "none";
-          var buttons = popup.getElementsByClassName( 'buttonsCustom' )[ 0 ];
-          buttons.style.display = "block";
-          while( buttons.firstChild )
-            buttons.removeChild( buttons.firstChild );
-          for ( var i in callbacks )
-          {
-            if ( i == "sound" )
-              continue;
-            b = document.createElement( "button" );
-            b.className = i;
-            b.i = i;
-            b.innerHTML = DE.Localization.get( i ) || i;
-            b.addEventListener( 'pointerup'
-            , function( e )
-            {
-              e.stopPropagation();
-              e.preventDefault();
-              var target = e.target;
-              while( target.tagName.toLowerCase() != "button" )
-                target = target.parentElement;
-              if ( callbacks.sound || _self.defaultSounds.default )
-                DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.default );
-              callbacks[ target.i ].call( contexts, e );
-              if ( closes.indexOf( target.i ) != -1 )
-                _self.remove( popup.id );
-              return false;
-            } );
-            buttons.appendChild( b );
-          }
-          break;
-
-        // generate a text field with cancel/ok buttons
-        case "textfield":
-            if ( contexts && !contexts.ok )
-              contexts = { ok: contexts, cancel: contexts };
-            if ( !contexts )
-              contexts = { ok: window, cancel: window };
-              
-            popup.getElementsByClassName( 'buttonsDefault' )[ 0 ].style.display = "none";
-            popup.getElementsByClassName( 'inputTextfield' )[ 0 ].style.display = "block";
-            popup.getElementsByClassName( 'buttonsTextfield' )[ 0 ].style.display = "block";
-
-            setTimeout( () => {
-              popup.getElementsByClassName( 'valueTextfield' )[ 0 ].focus();
-            }, 250);
-
-            var cancel = popup.getElementsByClassName( 'cancelTextfield' )[ 0 ];
-            cancel.innerHTML = DE.Localization.get( "cancel" ) || "cancel";
-            cancel.addEventListener( 'pointerup'
-            , function( e )
-            {
-              e.stopPropagation();
-              e.preventDefault();
-              if ( callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default )
-                DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default );
-              if ( callbacks.cancel )
-                callbacks.cancel.call( contexts.cancel );
-              _self.remove( popup.id );
-              return false;
-            } );
-            var ok = popup.getElementsByClassName( 'okTextfield' )[ 0 ];
-            ok.innerHTML = DE.Localization.get( "ok" ) || "ok";
-            ok.addEventListener( 'pointerup'
-            , function( e )
-            {
-              e.stopPropagation();
-              e.preventDefault();
-              if ( callbacks.sound || _self.defaultSounds.ok || _self.defaultSounds.default )
-                DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.ok || _self.defaultSounds.default );
-              if ( callbacks.ok )
-                callbacks.ok.call( contexts.ok, popup.getElementsByClassName( 'valueTextfield' )[ 0 ].value );
-              _self.remove( popup.id );
-              return false;
-            } );
-          break;
-        
-        default: // default is information with button ok
-          if ( !contexts )
-            contexts = window;
-          popup.getElementsByClassName( 'okBtn' )[ 0 ].addEventListener( 'pointerup'
+        break;
+      
+      // generate a button list
+      case "custom":
+        popup.getElementsByClassName('buttonsDefault')[ 0 ].style.display = "none";
+        var buttons = popup.getElementsByClassName('buttonsCustom')[ 0 ];
+        buttons.style.display = "block";
+        while( buttons.firstChild )
+          buttons.removeChild( buttons.firstChild );
+        for ( var i in callbacks )
+        {
+          if ( i == "sound" )
+            continue;
+          b = document.createElement( "button" );
+          b.className = i;
+          b.i = i;
+          b.innerHTML = DE.Localization.get( i ) || i;
+          b.addEventListener('pointerup'
           , function( e )
           {
             e.stopPropagation();
             e.preventDefault();
-            // in this case, closes is the sound
-            if ( closes || _self.defaultSounds.ok || _self.defaultSounds.default )
-              DE.Audio.fx.play( closes || _self.defaultSounds.ok || _self.defaultSounds.default );
-            if ( callbacks )
-              callbacks.call( contexts );
+            var target = e.target;
+            while( target.tagName.toLowerCase() != "button" )
+              target = target.parentElement;
+            if ( callbacks.sound || _self.defaultSounds.default )
+              DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.default );
+            callbacks[ target.i ].call( contexts, e );
+            if ( closes.indexOf( target.i ) != -1 )
+              _self.remove( popup.id );
+            return false;
+          } );
+          buttons.appendChild( b );
+        }
+        break;
+
+      // generate a text field with cancel/ok buttons
+      case "textfield":
+          if ( contexts && !contexts.ok )
+            contexts = { ok: contexts, cancel: contexts };
+          if ( !contexts )
+            contexts = { ok: window, cancel: window };
+            
+          popup.getElementsByClassName('buttonsDefault')[ 0 ].style.display = "none";
+          popup.getElementsByClassName('inputTextfield')[ 0 ].style.display = "block";
+          popup.getElementsByClassName('buttonsTextfield')[ 0 ].style.display = "block";
+
+          setTimeout( () => {
+            popup.getElementsByClassName('valueTextfield')[ 0 ].focus();
+          }, 250);
+
+          var cancel = popup.getElementsByClassName('cancelTextfield')[ 0 ];
+          cancel.innerHTML = DE.Localization.get('cancel') || "cancel";
+          cancel.addEventListener('pointerup'
+          , function( e )
+          {
+            e.stopPropagation();
+            e.preventDefault();
+            if ( callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default )
+              DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.no || _self.defaultSounds.default );
+            if ( callbacks.cancel )
+              callbacks.cancel.call( contexts.cancel );
             _self.remove( popup.id );
             return false;
           } );
-          break;
-      }
+          var ok = popup.getElementsByClassName('okTextfield')[ 0 ];
+          ok.innerHTML = DE.Localization.get('popup.ok') || DE.Localization.get('ok') || "ok";
+          ok.addEventListener('pointerup'
+          , function( e )
+          {
+            e.stopPropagation();
+            e.preventDefault();
+            if ( callbacks.sound || _self.defaultSounds.ok || _self.defaultSounds.default )
+              DE.Audio.fx.play( callbacks.sound || _self.defaultSounds.ok || _self.defaultSounds.default );
+            if ( callbacks.ok )
+              callbacks.ok.call( contexts.ok, popup.getElementsByClassName('valueTextfield')[ 0 ].value );
+            _self.remove( popup.id );
+            return false;
+          } );
+        break;
+      
+      default: // default is information with button ok
+        if ( !contexts )
+          contexts = window;
+        
+        var okBtn = popup.getElementsByClassName('okBtn')[ 0 ];
+        okBtn.innerHTML = DE.Localization.get('ok') || "ok";
+        okBtn.addEventListener('pointerup'
+        , function( e )
+        {
+          e.stopPropagation();
+          e.preventDefault();
+          // in this case, closes is the sound
+          if ( closes || _self.defaultSounds.ok || _self.defaultSounds.default )
+            DE.Audio.fx.play( closes || _self.defaultSounds.ok || _self.defaultSounds.default );
+          if ( callbacks )
+            callbacks.call( contexts );
+          _self.remove( popup.id );
+          return false;
+        } );
+        break;
+    }
       
     this.el.appendChild( popup );
     this.el.style.display = 'block';
